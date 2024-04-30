abafec06875fcd02638b145058dc377251a3247d
Julian Pompey <jupompey00@gmail.com>
2024-04-30 12:13:11 -0400

    updated present_values.js

--- scripts/present_value.js
+++ scripts/present_value.js
@@ -3,45 +3,41 @@ window.onload = init;
 
 
 //input fields
-const number1 = document.getElementById("number1");
-const interest = document.getElementById("interestRate");
-const loanLength = document.getElementById("time");
+const paymentPerMonthTextBox = document.getElementById("paymentPerMonthTextBox");
+const interestRateTextBox = document.getElementById("interestRateTextBox");
+const yearTextBox = document.getElementById("yearTextBox");
 
 //buttons
-const submitBtnMortgage = document.getElementById("submitBtnMortgage");
-const submitBtnFuture = document.getElementById("submitBtnFuture");
-const submitBtnPresent = document.getElementById("submitBtnPresent");
-const resetBtn = document.getElementById("resetBtn");
+const calculatePresentValueButton = document.getElementById("calculatePresentValueButton");
+const resetButton = document.getElementById("resetButton");
 
 //output element
-const outputElement = document.getElementById("output");
+const outputElement = document.getElementById("outputTextBox");
 
 
 function init() {
 
     //It checks which page it is on with these if blocks
 
-    submitBtnPresent.onclick = submitBtnPresentClicked;
+    calculatePresentValueButton.onclick = onCalculatePresentValueButtonClicked;
 
-    resetBtn.onclick = resetBtnClicked;
+    resetButton.onclick = onResetButtonClicked;
 }
 
 
-function submitBtnPresentClicked() {
+function onCalculatePresentValueButtonClicked() {
     //variables used for equations and output
-    let equation1;
-    let equation2;
-    let output;
+    let amountToInvest;
 
     //variables using the values from the document
-    let paymentPerMonth = Number(number1.value);
-    let interestRatePerPeriod = Number(interest.value) / 100;
-    let periods = Number(loanLength.value);
+    let paymentPerMonth = Number(paymentPerMonthTextBox.value);
+    let interestRatePerPeriod = Number(interestRateTextBox.value) / 100;
+    let yearNumber = Number(yearTextBox.value);
 
 
 
     //checks to make sure they inputted a number
-    if (Number.isNaN(paymentPerMonth) || Number.isNaN(interestRatePerPeriod) || Number.isNaN(periods)) {
+    if (Number.isNaN(paymentPerMonth) || Number.isNaN(interestRatePerPeriod) || Number.isNaN(yearNumber)) {
 
         //puts an error message where the output would go in red read-only text
         outputElement.value = "Please make sure you enter numbers in all fields";
@@ -50,21 +46,21 @@ function submitBtnPresentClicked() {
     } else {
 
 
-        equation1 = (1 - (1 + (interestRatePerPeriod / 12)) ** (-1 * (periods * 12)));
-        equation2 = equation1 / (interestRatePerPeriod / 12);
+        let equation1 = (1 - (1 + (interestRatePerPeriod / 12)) ** (-1 * (yearNumber * 12)));
+        let equation2 = equation1 / (interestRatePerPeriod / 12);
 
-        output = paymentPerMonth * equation2;
+        amountToInvest = paymentPerMonth * equation2;
         outputElement.setAttribute("class", "form-control-plaintext")
 
-        outputElement.value = `To fund an annuity that pays $${paymentPerMonth} monthly for ${periods} years and earns an expected ${interestRatePerPeriod * 100}% interest, you would need to invest $${output.toFixed(2)} today`;
+        outputElement.value = `To fund an annuity that pays $${paymentPerMonth} monthly for ${yearNumber} years and earns an expected ${interestRatePerPeriod * 100}% interest, you would need to invest $${amountToInvest.toFixed(2)} today`;
     }
 
 }
 
 //clears all fields
-function resetBtnClicked() {
-    number1.value = "";
-    interest.value = "";
-    loanLength.value = "";
+function onResetButtonClicked() {
+    paymentPerMonthTextBox.value = "";
+    interestRateTextBox.value = "";
+    yearTextBox.value = "";
     outputElement.value = "";
 }
